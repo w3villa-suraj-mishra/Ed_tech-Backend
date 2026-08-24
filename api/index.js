673e9118b63272327d2b80892f99ec1a4a38dc76
w3villa-suraj-mishra <suraj.mishra@w3villa.com>
2026-08-24 16:47:04 +0530

    Wrap Vercel serverless entrypoint in try-catch error handler

--- api/index.js
+++ api/index.js
@@ -1,3 +1,14 @@
 const app = require('../server');
 
-module.exports = app;
+module.exports = (req, res) => {
+  try {
+    return app(req, res);
+  } catch (error) {
+    console.error("Vercel Serverless Function Execution Error:", error);
+    return res.status(500).json({
+      success: false,
+      message: "Serverless Execution Error",
+      error: error.message
+    });
+  }
+};
