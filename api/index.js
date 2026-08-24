cc3dc16e62fb750cba76c130f191f7545a7cfa98
w3villa-suraj-mishra <suraj.mishra@w3villa.com>
2026-08-24 16:53:57 +0530

    Export direct Express app module in api/index.js

--- api/index.js
+++ api/index.js
@@ -1,14 +1,3 @@
 const app = require('../server');
 
-module.exports = (req, res) => {
-  try {
-    return app(req, res);
-  } catch (error) {
-    console.error("Vercel Serverless Function Execution Error:", error);
-    return res.status(500).json({
-      success: false,
-      message: "Serverless Execution Error",
-      error: error.message
-    });
-  }
-};
+module.exports = app;
